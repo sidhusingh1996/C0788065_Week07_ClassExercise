8c9c1ac061cd6cc4be203148ca33f2ad23b81de3
Paramveer <paramveersidhu0@gmail.com>
2021-03-06 11:50:49 -0500

    Array in reverse order

--- src/main/java/C0788065_Week07_ClassExercise/C0788065_Week07_ClassExercise.java
+++ src/main/java/C0788065_Week07_ClassExercise/C0788065_Week07_ClassExercise.java
@@ -1,5 +1,16 @@
 package C0788065_Week07_ClassExercise;
 
 public class C0788065_Week07_ClassExercise {
+    public static void main(String[] args) {
+        int[] array = {34, 23, 64, 66, 44, 1, 98, 39, 17, 87};
+        System.out.println("\n\nArray in correct order:");
+        for (int a : array) {
+            System.out.print(a + "  ");
+        }
 
+        System.out.println("\n\nArray in reverse order:");
+        for (int i = array.length - 1 ; i >= 0 ; i-- )  {
+            System.out.print(array[i] + "  ");
+        }
+    }
 }
